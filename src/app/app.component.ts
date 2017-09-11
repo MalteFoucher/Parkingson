0fc3e4221063e0a0a65c1e1c5bee8d0174b83418
Michael Schommer <michael@jatomix.de>
2017-09-11 16:36:19 +0200

    Sidenav fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -140,7 +140,7 @@ export class AppComponent implements AfterViewChecked {
 
   setContent(content) {
     this.content = content;
-    this.sidenav.close();
+    this.ngZone.run(() => this.sidenav.close());
   }
 
 }
